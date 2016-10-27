147cd1ae54fae01b6f148cacde7c017d1f893f8e
LuisPlata94 <luisf.pg11@gmail.com>
2016-10-27 16:26:49 -0500

    426 jueves

--- ICP2.m
+++ ICP2.m
@@ -36,12 +36,12 @@ xi = irutas(nc); %Plata: seria cuestion de modificar irutas para
 
 %% Llamado a Recocido
 clc;
-c0 = 2400;               % temperatura inicial
+c0 = 100;               % temperatura inicial
 p.cadIntAcep = 100;
 p.cadInt = 400;
 p.maxCad = 3;
 p.frecImp = 50;
-p.alfa = 0.98;
+p.alfa = 0.99;
 p.variarC = 0;
 % para optimizar, se guarda en la x la solucion (las rutas) y el vector con
 % los costos de cada ruta.
